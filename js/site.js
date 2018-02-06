103198384be7376e99f12240ba3108b5bd335400
rmai <rmai@hawk.iit.edu>
2018-02-06 00:59:49 -0600

    edit js for what will happen when form is submitted

--- js/site.js
+++ js/site.js
@@ -2,7 +2,9 @@ $('#contact-form').on('submit',
   function() {
     console.log('The form was submitted.');
     var email = $('#email').val();
+    var firstname = $('#firstname').val();
+    var lastname = $('#lastname').val();
     $(this).remove();
-    $('body').append('<h1>Thank You. Email Address: ' + email + '</h1>');
+    $('body').append('<h1>Thank You, ' + firstname + ' ' + lastname + ' for your email address: ' + email + '</h1>');
   }
 );
