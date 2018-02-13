9cd377ccbe049a21d2675a7f259d972b38aeb261
rmai <rmai@hawk.iit.edu>
2018-02-13 15:33:25 -0600

    fix form submit function to match new id of name

--- js/site.js
+++ js/site.js
@@ -2,8 +2,7 @@ $('#contact-form').on('submit',
   function() {
     console.log('The form was submitted.');
     var email = $('#email').val();
-    var firstname = $('#firstname').val();
-    var lastname = $('#lastname').val();
+    var name = $('#name').val();
     $(this).remove();
     $('body').append('<h1>Thank You, ' + firstname + ' ' + lastname + ' for your email address: ' + email + '</h1>');
     $('#instruct').remove();
