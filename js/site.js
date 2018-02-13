207da12020724ae1bedc83e34340f6ca85e4052e
rmai <rmai@hawk.iit.edu>
2018-02-13 16:09:21 -0600

    make where when email and name isnt entered and error message appears

--- js/site.js
+++ js/site.js
@@ -1,11 +1,18 @@
 $('#contact-form').on('submit',
   function() {
-    console.log('The form was submitted.');
-    var email = $('#email').val();
-    var name = $('#name').val();
-    $(this).remove();
-    $('body').append('<h1>Thank You, ' + name +  ' for your email address: ' + email + '</h1>');
-    $('#instruct').remove();
-    $('#footer').remove();
+    //when submit un successfully
+    if (document.getElementById("name").value == "" && document.getElementById("email").value == "" ) {
+      window.alert("Please enter your name and email");
+    }
+    //when submit successfully
+    else {
+      console.log('The form was submitted.');
+      var email = $('#email').val();
+      var name = $('#name').val();
+      $(this).remove();
+      $('body').append('<h1>Thank You, ' + name +  ' for your email address: ' + email + '</h1>');
+      $('#instruct').remove();
+      $('#footer').remove();
+    }
   }
 );
