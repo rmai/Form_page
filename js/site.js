656427103f5c07d569ad13babcb7169d1da4587b
Randy Mai <randymai@yahoo.com>
2018-02-14 12:30:58 -0600

    add red border to input when missing information

--- js/site.js
+++ js/site.js
@@ -1,14 +1,18 @@
 $('#contact-form').on('submit',
   function() {
     //when submit un successfully
-    if (document.getElementById("name").value == "" && document.getElementById("email").value == "" ) {
+    if (document.getElementById("name").value === "" && document.getElementById("email").value == "" ) {
       window.alert("Please enter your name and email");
+      document.getElementById("name").style.borderColor = "red";
+      document.getElementById("email").style.borderColor = "red";
     }
-    else if (document.getElementById("name").value=="") {
+    else if (document.getElementById("name").value =="") {
       window.alert("Please enter your name");
+      document.getElementById("name").style.borderColor = "red";
     }
     else if (document.getElementById("email").value=="") {
       window.alert("Please enter your email");
+      document.getElementById("email").style.borderColor = "red";
     }
     //when submit successfully
     else {
