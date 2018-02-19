09109bcb56cc78fe163d354999643baf16bff959
Randy Mai <randymai@yahoo.com>
2018-02-18 22:37:29 -0600

    eslint file

--- js/site.js
+++ js/site.js
@@ -1,28 +1,28 @@
 $('#contact-form').on('submit',
   function() {
-  //when submit un successfully
-    if (document.getElementById("name").value === "" && document.getElementById("email").value == "" ) {
-    //window.alert("Please enter your name and email");
+    var email = $('#email').val();
+    var name = $('#name').val();
+    // when submit un successfully
+    if (document.getElementById("name").value === "" && document.getElementById("email").value === "" ) {
+    // window.alert("Please enter your name and email");
       document.getElementById("name").style.borderColor = "red";
       document.getElementById("email").style.borderColor = "red";
       $('#nameblock').append('<p id="ename">Please enter your name</p>');
       $('#emailblock').append('<p id="eemail">Please enter your email</p>');
     }
-    else if (document.getElementById("name").value =="") {
-    //window.alert("Please enter your name");
+    else if (document.getElementById("name").value ==="") {
+    // window.alert("Please enter your name");
       document.getElementById("name").style.borderColor = "red";
       $('#nameblock').append('<p id="ename">Please enter your name</p>');
     }
-    else if (document.getElementById("email").value=="") {
-    //window.alert("Please enter your email");
+    else if (document.getElementById("email").value==="") {
+    // window.alert("Please enter your email");
       document.getElementById("email").style.borderColor = "red";
       $('#emailblock').append('<p id="eemail">Please enter your email</p>');
     }
-    //when submit successfully
+    // when submit successfully
     else {
       console.log('The form was submitted.');
-      var email = $('#email').val();
-      var name = $('#name').val();
       $(this).remove();
       $('body').append('<h1>Thank You, ' + name +  ' for your email address: ' + email + '</h1>');
       $('#instruct').remove();
